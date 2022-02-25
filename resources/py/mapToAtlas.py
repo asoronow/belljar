162383d5036d6b5a88277fa213c924265a50830b
unknown <a.soronow@gmail.com>
2022-02-25 15:19:18 -0800

    began tf model

--- resources/py/mapToAtlas.py
+++ resources/py/mapToAtlas.py
@@ -1,5 +1,77 @@
-import tensorflow.keras as tf
-
+import os, numpy, cv2
+from pathlib import Path
+from simplejson import load
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.python.keras.regularizers import L2
+from tensorflow.python.keras.optimizers import adam_v2
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split
 # Links in case we should need to redownload these, will not be included
 nisslDownloadLink = "http://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf/ara_nissl/ara_nissil_10.nrrd"
 annotationDownloadLink = "http://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf/annotation/ccf_2017/annotation_10.nrrd"
+
+# TODO: Remove this test case, build handlers for this
+pathParent = Path(__file__)
+pngPath = os.path.join(pathParent.parents[1], "nrrd/png")
+fileList = os.listdir(os.path.join(pathParent.parents[1], "nrrd/png")) # path to flat pngs
+absolutePaths = [os.path.abspath(os.path.join(pngPath, name)) for name in fileList]
+
+train, test = train_test_split(absolutePaths, test_size=0.2)
+def loadImages(paths):
+    '''Takes an list of image file paths, returns a list of numpy arrays containing image data'''
+    images = []
+    for p in paths:
+        img = cv2.imread(p)
+        img = numpy.array(img, dtype=numpy.uint8)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+        images.append(img)
+    return images
+
+train = loadImages(train)
+test = loadImages(test)
+
+def encoder_decoder_model():
+
+  """
+  Used to build Convolutional Autoencoder model architecture to get compressed image data which is easier to process.
+  Returns:
+  Auto encoder model
+  """
+  #Encoder 
+  model = Sequential(name='Convolutional_AutoEncoder_Model')
+  model.add(Conv2D(64, kernel_size=(3, 3),activation='relu',input_shape=(800, 1140, 3),padding='same', name='Encoding_Conv2D_1'))
+  model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', name='Encoding_MaxPooling2D_1'))
+  model.add(Conv2D(128, kernel_size=(3, 3),strides=1,kernel_regularizer = L2(0.001),activation='relu',padding='same', name='Encoding_Conv2D_2'))
+  model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', name='Encoding_MaxPooling2D_2'))
+  model.add(Conv2D(256, kernel_size=(3, 3), activation='relu',kernel_regularizer= L2(0.001), padding='same', name='Encoding_Conv2D_3'))
+  model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', name='Encoding_MaxPooling2D_3'))
+  model.add(Conv2D(512, kernel_size=(3, 3), activation='relu',kernel_regularizer= L2(0.001), padding='same', name='Encoding_Conv2D_4'))
+  model.add(MaxPooling2D(pool_size=(2, 2), strides=2,padding='valid', name='Encoding_MaxPooling2D_4'))
+  model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='Encoding_Conv2D_5'))
+  model.add(MaxPooling2D(pool_size=(2,2), strides=2, padding='valid'))
+    
+  #Decoder
+  model.add(Conv2D(512, kernel_size=(3, 3), kernel_regularizer = L2(0.001),activation='relu', padding='same', name='Decoding_Conv2D_1'))
+  model.add(UpSampling2D((2, 2), name='Decoding_Upsamping2D_1'))
+  model.add(Conv2D(512, kernel_size=(3, 3), kernel_regularizer = L2(0.001), activation='relu', padding='same', name='Decoding_Conv2D_2'))
+  model.add(UpSampling2D((2, 2), name='Decoding_Upsamping2D_2'))
+  model.add(Conv2D(256, kernel_size=(3, 3), kernel_regularizer = L2(0.001), activation='relu', padding='same',name='Decoding_Conv2D_3'))
+  model.add(UpSampling2D((2, 2),name='Decoding_Upsamping2D_3'))
+  model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', kernel_regularizer = L2(0.001), padding='same',name='Decoding_Conv2D_4'))
+  model.add(UpSampling2D((2, 2),name='Decoding_Upsamping2D_4'))
+  model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', kernel_regularizer = L2(0.001), padding='same',name='Decoding_Conv2D_5'))
+  model.add(UpSampling2D((2, 2),name='Decoding_Upsamping2D_5'))
+  model.add(Conv2D(3, kernel_size=(3, 3), padding='same',activation='sigmoid',name='Decoding_Output'))
+  
+  return model
+    
+model = encoder_decoder_model()
+model.summary()
+optimizer = adam_v2.Adam(learning_rate=0.001) 
+model = encoder_decoder_model() 
+model.compile(optimizer=optimizer, loss='mse') 
+early_stopping = EarlyStopping(monitor='val_loss', mode='min',verbose=1,patience=6,min_delta=0.0001) 
+checkpoint = ModelCheckpoint(os.path.join(pathParent.parents[0],'encoder_model.h5'), monitor='val_loss', mode='min', save_best_only=True) 
+model.fit(train, train, epochs=35, batch_size=32,validation_data=(test,test),callbacks=[early_stopping,checkpoint]) 
