change dump for rc prep, masking, stablity fixes

py/find_neurons.py
@@ -1,12 +1,14 @@
 import cv2
 import pickle
 import os
-from ultralytics import YOLO 
+from ultralytics import YOLO
 from torchvision.ops import nms
 import torch
 import numpy as np
 import argparse
 from pathlib import Path
+from skimage import morphology
+
 parser = argparse.ArgumentParser(description="Find neurons in images")
 parser.add_argument(
     "-o", "--output", help="output directory, only use if graphical false", default=""
@@ -44,14 +46,23 @@ if __name__ == "__main__":
 
     for file in files:
         filePath = os.path.join(inputDirectory, file)
+        print(f"Processing {file}", flush=True)
         try:
-            img = cv2.imread(filePath)
+            img = cv2.imread(filePath, cv2.IMREAD_GRAYSCALE)
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(15, 15))
+            img = clahe.apply(img)
+            display = img.copy()
+            display = cv2.cvtColor(display, cv2.COLOR_GRAY2RGB)
+
+            selem = morphology.disk(20)
+            img = morphology.white_tophat(img, selem)
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
         except:
             print(f"Error reading {file}", flush=True)
             continue
-        # Adjust image
 
-        print(f"Processing {file}", flush=True)
+        # Adjust image
         height, width = img.shape[:2]
 
         predictionImage = np.zeros((height, width, 3))
@@ -63,7 +74,7 @@ if __name__ == "__main__":
         # Initialize an empty list to hold all predictions
         pred = []
         tiles = []
-        overlap = 0.1
+        overlap = 0.2
         overlap_px = int(tileSize * overlap)  # overlap in pixels
 
         tile_coords = []
@@ -78,21 +89,27 @@ if __name__ == "__main__":
 
                 tile = img[y1:y2, x1:x2]  # Extract the tile from the image
 
-                # tophat filter
-                tile = cv2.morphologyEx(tile, cv2.MORPH_TOPHAT, (15, 15))
-
                 tiles.append(tile)
                 tile_coords.append((x1, y1, x2, y2))
 
-        results = model.predict(tiles, conf=0.5)
+        dev = "cuda" if torch.cuda.is_available() else "cpu"
+        if dev == "cpu":
+            # check if MPS is enabled
+            try:
+                if torch.backends.mps.is_available():
+                    dev = "mps"
+            except AttributeError:
+                pass
+
+        results = model.predict(tiles, conf=0.15, device=dev)
         for i, r in enumerate(results):
             boxes = r.boxes.cpu().numpy()
             scores = boxes.conf
             tile_x1, tile_y1, _, _ = tile_coords[i]
-            
+
             for j, box in enumerate(boxes):
                 x1, y1, x2, y2 = box.xyxy[0].astype(int)
-                
+
                 # Adjust the bounding box coordinates to account for the tile offset
                 x1 += tile_x1
                 y1 += tile_y1
@@ -103,16 +120,26 @@ if __name__ == "__main__":
                 pred.append((x1, y1, x2, y2, scores[j]))
 
         # Non-maximum suppression
-        pred = np.array(pred)
-        keep = nms(
-            torch.tensor(pred[:, :4], dtype=torch.float32),
-            torch.tensor(pred[:, 4], dtype=torch.float32),
-            0.4,
-        )
+        if len(pred) == 0:
+            pass
+        else:
+            pred = np.array(pred)
+            keep = nms(
+                torch.tensor(pred[:, :4], dtype=torch.float32),
+                torch.tensor(pred[:, 4], dtype=torch.float32),
+                0.4,
+            )
 
-        for p in pred[keep]:
-            p = p.astype(int)
-            cv2.rectangle(img, (p[0], p[1]), (p[2], p[3]), (0, 0, 255), 2)            
+            for p in pred[keep]:
+                p = p.astype(int)
+                cv2.rectangle(display, (p[0], p[1]), (p[2], p[3]), (0, 0, 255), 2)
+                cv2.circle(
+                    predictionImage,
+                    (int((p[0] + p[2]) / 2), int((p[1] + p[3]) / 2)),
+                    5,
+                    (0, 0, 255),
+                    -1,
+                )
 
         # No extension filename
         stripped = file.split(".")[0]
@@ -121,7 +148,7 @@ if __name__ == "__main__":
         cv2.imwrite(
             os.path.join(outputDirectory, f"Dots_{stripped}.png"), predictionImage
         )
-        cv2.imwrite(os.path.join(outputDirectory, f"BBoxes_{stripped}.png"), img)
+        cv2.imwrite(os.path.join(outputDirectory, f"BBoxes_{stripped}.png"), display)
 
         # Write the raw results to a pkl for later review or reuse
         with open(

py/map.py
@@ -70,6 +70,7 @@ def save_alignment(
     file_list,
     angle,
     input_dir,
+    masks=None,
 ):
     """Writes the selected atlas section for each tissue section to a simple text file"""
     with open(Path(input_dir.strip()) / "alignment.txt", "w") as f:
@@ -78,6 +79,22 @@ def save_alignment(
             current_file = file_list[i]
             s_r = selected_sections_right[current_file]
             f.write(f"{current_file}${s}${s_r}${r}\n")
+
+    if masks is not None:
+        with open(Path(input_dir.strip()) / "masks.pkl", "wb") as f:
+            pickle.dump(masks, f)
+
+
+def load_masks(input_dir):
+    """Load masks if any from the input directory"""
+    try:
+        print("Loading prior masks...", flush=True)
+        with open(Path(input_dir.strip()) / "masks.pkl", "rb") as f:
+            return pickle.load(f)
+    except:
+        # bad file or no file, just give no masks
+        print("No compatible prior masks found...", flush=True)
+        return {}
 
 
 def load_alignment(input_dir, file_list):
@@ -177,7 +194,7 @@ if __name__ == "__main__":
         for i, image in enumerate(new_sections):
             predictions[image] = new_predictions[image]
             right_hemisphere_steps[image] = new_predictions[image]
-    
+
     else:
         print("Making predictions...", flush=True)
         # no saved values, make a fresh prediction
@@ -195,7 +212,8 @@ if __name__ == "__main__":
     # Globals for alignment
     left_section_num = predictions[fileList[0]]
     right_section_num = right_hemisphere_steps[fileList[0]]
-    atlas_masks = {}
+    atlas_masks = load_masks(args.input)
+
     def adjustPredictions(predictions, right_predictions, visited, fileList):
         global args
 
@@ -363,7 +381,7 @@ if __name__ == "__main__":
             atlasTypeDropdown.setCurrentIndex(1)
         elif flag == "NC":
             atlasTypeDropdown.setCurrentIndex(2)
-    
+
     def get_current_atlas():
         global left_section_num, right_section_num, is_whole, atlas
         if is_whole:
@@ -372,64 +390,64 @@ if __name__ == "__main__":
             return atlas[left_section_num, :, :], None
 
     def set_mask():
-        global atlas_masks, currentSection
+        global atlas_masks, currentSection, addMask
 
         left_atlas, right_atlas = get_current_atlas()
         if right_atlas is not None:
-            mask = paint_mask(np.concatenate((left_atlas, right_atlas), axis=1))
+            mask = paint_and_get_mask(np.concatenate((left_atlas, right_atlas), axis=1))
         else:
-            mask = paint_mask(left_atlas)
+            mask = paint_and_get_mask(left_atlas)
 
         atlas_masks[fileList[currentSection]] = mask
+        addMask.setText("Change Mask")
 
-    def paint_mask(image):
-        # Create a clone of the image to work on
-        clone = image.copy()
-        # Create a mask with 0s
-        mask = np.zeros((image.shape[0], image.shape[1]))
+    def paint_and_get_mask(img):
+        drawing = False
+        pts = []
 
-        # List to store history of drawn segments for the undo functionality
-        segments_history = []
+        # ensure image is color
+        if len(img.shape) == 2:
+            # convert to 8bit from float64
+            img = (img).astype(np.uint8)
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-        # The function to draw the segment on the image and mask
-        def draw_segment(event, x, y, flags, param):
-            nonlocal segments_history
+        def draw_circle(event, x, y, flags, param):
+            nonlocal drawing, pts
             if event == cv2.EVENT_LBUTTONDOWN:
-                segments_history.append((x, y))
-            elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
-                if segments_history:
-                    cv2.line(clone, segments_history[-1], (x, y), (0, 0, 255), 2)
-                    cv2.line(mask, segments_history[-1], (x, y), 1, 2)
-                    segments_history.append((x, y))
-                    cv2.imshow('Image', clone)
+                drawing = True
+                pts.append((x, y))
+            elif event == cv2.EVENT_MOUSEMOVE:
+                if drawing:
+                    cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+                    pts.append((x, y))
+            elif event == cv2.EVENT_LBUTTONUP:
+                drawing = False
+                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+                pts.append((x, y))
 
-        # Setting the mouse callback function to draw the segment
-        cv2.namedWindow('Image')
-        cv2.setMouseCallback('Image', draw_segment)
+        # Load image
+        if img is None:
+            print(f"Could not open or find the image")
+            return None
 
-        while True:
-            # Display the image
-            cv2.imshow('Image', clone)
-            key = cv2.waitKey(1) & 0xFF
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
 
-            # If 'q' is pressed, break from the loop
-            if key == ord("q"):
+        cv2.namedWindow("Image")
+        cv2.setMouseCallback("Image", draw_circle)
+
+        while 1:
+            cv2.imshow("Image", img)
+            k = cv2.waitKey(1) & 0xFF
+            if k == ord("q"):
                 break
-            # If 'z' is pressed, undo the last segment
-            elif key == ord("z"):
-                if segments_history:
-                    clone = image.copy()
-                    mask = np.zeros((image.shape[0], image.shape[1]))
-                    segments_history.pop()
-                    for i in range(0, len(segments_history), 2):
-                        if i + 1 < len(segments_history):
-                            cv2.line(clone, segments_history[i], segments_history[i + 1], (0, 0, 255), 2)
-                            cv2.line(mask, segments_history[i], segments_history[i + 1], 1, 2)
-                    cv2.imshow('Image', clone)
 
-        # Destroy all windows
+        # Filling the closed shape
+        if len(pts) > 2:
+            cv2.fillPoly(mask, [np.array(pts)], 1)
+
+        mask = np.logical_not(mask).astype(np.uint8)
+
         cv2.destroyAllWindows()
-
         return mask
 
     def nextSection():
@@ -448,6 +466,12 @@ if __name__ == "__main__":
             progressBar.setValue(currentSection + 1)
             left_section_num = predictions[fileList[currentSection]]
             right_section_num = right_hemisphere_steps[fileList[currentSection]]
+
+            # If there is a mask set the addMask button text to Change Mask
+            if fileList[currentSection] in list(atlas_masks.keys()):
+                addMask.setText("Change Mask")
+            else:
+                addMask.setText("Add Mask")
 
             sectionLayer.data = cv2.resize(
                 images[currentSection],
@@ -484,6 +508,13 @@ if __name__ == "__main__":
                 images[currentSection],
                 (atlas.shape[2], atlas.shape[1]),
             )
+
+            # If there is a mask set the addMask button text to Change Mask
+            if fileList[currentSection] in list(atlas_masks.keys()):
+                addMask.setText("Change Mask")
+            else:
+                addMask.setText("Add Mask")
+
             link_hemispheres.setChecked(is_linked[fileList[currentSection]])
             right_hemi_slider.blockSignals(True)
             right_hemi_slider.setValue(right_section_num)
@@ -495,9 +526,11 @@ if __name__ == "__main__":
 
     def finishAlignment():
         """Save our final updated prediction, perform warps, close, also write atlas borders to file"""
-        global currentSection, isProcessing, angle, predictions, region_selections
+        global currentSection, isProcessing, angle, predictions, region_selections, atlas_masks
+
         if isProcessing:
             return
+
         print("Warping output...", flush=True)
         isProcessing = True
 
@@ -509,6 +542,7 @@ if __name__ == "__main__":
             fileList,
             angle,
             args.input,
+            masks=atlas_masks,
         )
 
         # Load the appropriate annotation
@@ -578,6 +612,13 @@ if __name__ == "__main__":
                 section = used_atlas[int(predictions[imageName]), :, :]
 
             tissue = images[i]
+
+            # Check if tissue has a mask
+            if imageName in atlas_masks.keys():
+                mask = atlas_masks[imageName]
+                section = section * mask
+                label = label * mask
+
             # resize atlas and label to match tissue
             section = cv2.resize(section, (tissue.shape[1], tissue.shape[0]))
             label = cv2.resize(
@@ -585,6 +626,7 @@ if __name__ == "__main__":
                 (tissue.shape[1], tissue.shape[0]),
                 interpolation=cv2.INTER_NEAREST,
             )
+
             warped_labels, warped_atlas, color_label = register_to_atlas(
                 tissue,
                 section,
@@ -659,7 +701,7 @@ if __name__ == "__main__":
     backButton = QPushButton("Previous Section")
     doneButton = QPushButton("Done")
     addMask = QPushButton("Add Mask")
-    
+
     progressBar = QProgressBar(minimum=1, maximum=len(images))
     progressBar.setFormat(f"1/{len(images)}")
     progressBar.setValue(1)
@@ -668,7 +710,7 @@ if __name__ == "__main__":
     nextButton.clicked.connect(nextSection)
     backButton.clicked.connect(prevSection)
     doneButton.clicked.connect(finishAlignment)
-    addMask.clicked.connect(lambda: paint_mask(inputPath / fileList[currentSection]))
+    addMask.clicked.connect(set_mask)
     # Dropdown to select if it should be cerebrum only, no cerebrum, or whole brain
     atlasTypeDropdown = QComboBox()
     atlasTypeDropdown.addItem("All Regions")
@@ -695,7 +737,14 @@ if __name__ == "__main__":
     right_hemi_slider.valueChanged.connect(set_right_section)
 
     bottomRow = [left_hemi_label, left_hemi_slider, left_hemi_value]
-    widgets = [progressBar, atlasTypeDropdown, nextButton, backButton, doneButton]
+    widgets = [
+        progressBar,
+        atlasTypeDropdown,
+        addMask,
+        nextButton,
+        backButton,
+        doneButton,
+    ]
     # Link left and right hemispheres
     link_hemispheres = QCheckBox("Link Hemispheres")
     link_hemispheres.setChecked(is_linked[fileList[currentSection]])
@@ -703,6 +752,10 @@ if __name__ == "__main__":
         widgets.insert(1, link_hemispheres)
         extras = [right_hemi_label, right_hemi_slider, right_hemi_value]
         bottomRow.extend(extras)
+
+    # check if first section has loaded mask
+    if fileList[currentSection] in list(atlas_masks.keys()):
+        addMask.setText("Change Mask")
 
     # Add the buttons to the dock
     viewer.window.add_dock_widget(

py/max.py
@@ -1,11 +1,11 @@
 from tkinter import filedialog
-import tifffile as tf
 import os
+import tifffile as tiff
 from skimage.filters import unsharp_mask
-from skimage import filters, morphology
 import tkinter as tk
 import numpy as np
 import argparse
+import cv2
 
 parser = argparse.ArgumentParser(description="Process z-stack images")
 parser.add_argument(
@@ -40,8 +40,7 @@ def process_file(file, outputDirectory, topHat=False, dendrite=False):
     # Update current file
     try:
         print(f"Processing {file}", flush=True)
-        img = tf.imread(file)
-
+        img = tiff.imread(file)
         # Max projection
         img = np.max(img, axis=0)
 
@@ -61,7 +60,7 @@ def process_file(file, outputDirectory, topHat=False, dendrite=False):
         stem = file.split(".")[0]
 
         # Save the processed image
-        tf.imwrite(f"{outputDirectory}/{stem}.tif", img)
+        cv2.imwrite(f"{outputDirectory}/{stem}.tif", img)
     except Exception as e:
         print(f"Failed to process {file}. Error: {e}", flush=True)
 