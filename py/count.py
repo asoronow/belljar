459abc405c42008df0af7619d923b0d8f0776b9c
asoronow <a.soronow@gmail.com>
2023-09-18 16:47:56 -0700

    fix prediction and count break, add density info

--- py/count.py
+++ py/count.py
@@ -69,59 +69,85 @@ if __name__ == "__main__":
             nameToRegion[row[2]] = int(row[0])
 
     sums = {}
+    region_areas = {}
     for i, pName in enumerate(predictionFiles):
         # divide up the results file by section as well
         sums[annotationFiles[i][11:]] = {}
-        currentSection = sums[annotationFiles[i][11:]]
+        region_areas[annotationFiles[i][11:]] = {}
+        local_sums = sums[annotationFiles[i][11:]]
+        local_region_areas = region_areas[annotationFiles[i][11:]]
         with open(predictionPath / pName, "rb") as predictionPkl, open(
             annotationPath / annotationFiles[i], "rb"
         ) as annotationPkl:
             print("Counting...", flush=True)
-            prediction = pickle.load(predictionPkl)
-            predictedSize = prediction.pop()
+            predictions = pickle.load(predictionPkl)
+            predictedSize = predictions.pop()
+            predictions = predictions[0].astype(int).tolist()
             annotation = pickle.load(annotationPkl)
             height, width = annotation.shape
-            for p in prediction:
-                x, y, mX, mY = p.bbox.minx, p.bbox.miny, p.bbox.maxx, p.bbox.maxy
+
+
+            # Count the area of each region in the annotation
+            annotation_rescaled = cv2.resize(annotation.astype(np.int32), (predictedSize[1], predictedSize[0]), interpolation=cv2.INTER_NEAREST)
+            unique_ids, counts = np.unique(annotation_rescaled, return_counts=True)
+            for unique_id, count in zip(unique_ids, counts):
+                name = regions[unique_id]["name"]
+                parent_id = regions[unique_id]["parent"]
+                parent_name = regions[parent_id]["name"] if "layer" in name.lower() else name
+                local_region_areas[parent_name] = local_region_areas.get(parent_name, 0) + count
+
+            for p in predictions:
+                x, y, mX, mY = p[0], p[1], p[2], p[3]
                 xPos = (
-                    int((mX - (mX - x) // 2) * ((width) / predictedSize[1]))
+                    int((mX - (mX - x) // 2))
                 )
                 yPos = (
-                    int((mY - (mY - y) // 2) * ((height) / predictedSize[0]))
+                    int((mY - (mY - y) // 2))
                 )
-                atlasId = int(annotation[yPos, xPos])
-                name = regions[atlasId]["name"]
+                atlas_id = int(annotation_rescaled[yPos, xPos])
+                name = regions[atlas_id]["name"]
                 if "layer" in name.lower():
-                    parent = regions[atlasId]["parent"]
-                    name = regions[parent]["name"]
-                    if currentSection.get(name, False):
-                        currentSection[name] += 1
+                    atlas_id = regions[atlas_id]["parent"]
+                    name = regions[atlas_id]["name"]
+                    if local_sums.get(name, False):
+                        local_sums[name] += 1
                     else:
-                        currentSection[name] = 1
+                        local_sums[name] = 1
                 else:
-                    if currentSection.get(name, False):
-                        currentSection[name] += 1
+                    if local_sums.get(name, False):
+                        local_sums[name] += 1
                     else:
-                        currentSection[name] = 1
+                        local_sums[name] = 1
 
     with open(outputPath / "count_results.csv", "w", newline="") as resultFile:
         print("Writing output...", flush=True)
         lines = []
-        runningTotals = {}
-        for section, counts in sums.items():
+        running_counts = {}
+        running_areas = {}
+        sections = list(sums.keys())
+        counts = list(sums.values())
+        areas = list(region_areas.values())
+        for section, counts, areas in zip(sections, counts, areas):
             lines.append([section])
+            lines.append(["Region", "Acronym", "Count", "Area (px^2)"])
             for r, count in counts.items():
-                if runningTotals.get(r, False):
-                    runningTotals[r] += count
+                if running_counts.get(r, False):
+                    running_counts[r] += count
                 else:
-                    runningTotals[r] = count
+                    running_counts[r] = count
+                
+                if running_areas.get(r, False):
+                    running_areas[r] += areas.get(r, 0)
+                else:
+                    running_areas[r] = areas.get(r, 0)
 
-                lines.append([r, regions[nameToRegion[r]]["acronym"], count])
+                lines.append([r, regions[nameToRegion[r]]["acronym"], count, areas.get(r, 0)])
             lines.append([])
 
         lines.append(["Totals"])
-        for r, count in runningTotals.items():
-            lines.append([r, regions[nameToRegion[r]]["acronym"], count])
+        lines.append(["Region", "Acronym", "Count", "Area (px^2)"])
+        for r, count in running_counts.items():
+            lines.append([r, regions[nameToRegion[r]]["acronym"], count, running_areas.get(r, 0)])
         # Write out the rows
         resultWriter = csv.writer(resultFile)
         resultWriter.writerows(lines)
