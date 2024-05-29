0352c87e39fd270e27edb001f14a6d849840e62f
Alec <a.soronow@gmail.com>
2024-05-28 17:21:50 -0700

    update new model architecture

--- main/background.ts
+++ main/background.ts
@@ -7,39 +7,39 @@ import { setupHandlers } from "./helpers/handlers";
 const isProd = process.env.NODE_ENV === "production";
 let mainWindow: Electron.BrowserWindow;
 if (isProd) {
-  serve({ directory: "app" });
+	serve({ directory: "app" });
 } else {
-  app.setPath("userData", `${app.getPath("userData")} (development)`);
+	app.setPath("userData", `${app.getPath("userData")} (development)`);
 }
 
 (async () => {
-  await app.whenReady();
+	await app.whenReady();
 
-  mainWindow = createWindow("main", {
-    width: 1000,
-    height: 600,
-    autoHideMenuBar: true,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+	mainWindow = createWindow("main", {
+		width: 1000,
+		height: 600,
+		autoHideMenuBar: true,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+	});
 
-  if (isProd) {
-    await mainWindow.loadURL("app://./start");
-  } else {
-    const port = process.argv[2];
-    await mainWindow.loadURL(`http://localhost:${port}/start`);
-    mainWindow.webContents.openDevTools();
-  }
+	if (isProd) {
+		await mainWindow.loadURL("app://./start");
+	} else {
+		const port = process.argv[2];
+		await mainWindow.loadURL(`http://localhost:${port}/start`);
+		mainWindow.webContents.openDevTools();
+	}
 
-  const pythonScriptsPath: string = isProd
-    ? path.join(process.resourcesPath, "py")
-    : path.join(__dirname, "../py");
+	const pythonScriptsPath: string = isProd
+		? path.join(process.resourcesPath, "py")
+		: path.join(__dirname, "../py");
 
-  setupHandlers(pythonScriptsPath);
-  performSetup(mainWindow, pythonScriptsPath);
+	setupHandlers(pythonScriptsPath);
+	performSetup(mainWindow, pythonScriptsPath);
 })();
 
 app.on("window-all-closed", () => {
-  app.quit();
+	app.quit();
 });
