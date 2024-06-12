b2b500587e50c926a0ce4430dd136dce2d139213
Alec Soronow <a.soronow@gmail.com>
2024-06-11 21:45:53 -0700

    add dropout, scheduler, noise for training

--- py/deep_registration.py
+++ py/deep_registration.py
@@ -21,61 +21,41 @@ class BrainRegNet(nn.Module):
         super(BrainRegNet, self).__init__()
 
         features = init_features
-        self.encoder1 = BrainRegNet._block(in_channels, features, name="enc1")
+        self.encoder1 = BrainRegNet._block(in_channels, features, name="enc1", dropout=0.3)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = BrainRegNet._block(features, features * 2, name="enc2")
+        self.encoder2 = BrainRegNet._block(features, features * 2, name="enc2", dropout=0.3)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = BrainRegNet._block(features * 2, features * 4, name="enc3")
+        self.encoder3 = BrainRegNet._block(features * 2, features * 4, name="enc3", dropout=0.3)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = BrainRegNet._block(features * 4, features * 8, name="enc4")
+        self.encoder4 = BrainRegNet._block(features * 4, features * 8, name="enc4", dropout=0.3)
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.bottleneck = BrainRegNet._block(features * 8, features * 16, name="bottleneck")
+        self.bottleneck = BrainRegNet._block(features * 8, features * 16, name="bottleneck", dropout=0.3)
         
         self.upconv4 = nn.ConvTranspose2d(features * 16, features * 8, kernel_size=2, stride=2)
-        self.decoder4 = BrainRegNet._block((features * 8) * 2, features * 8, name="dec4")
+        self.decoder4 = BrainRegNet._block((features * 8) * 2, features * 8, name="dec4", dropout=0.3)
         self.upconv3 = nn.ConvTranspose2d(features * 8, features * 4, kernel_size=2, stride=2)
-        self.decoder3 = BrainRegNet._block((features * 4) * 2, features * 4, name="dec3")
+        self.decoder3 = BrainRegNet._block((features * 4) * 2, features * 4, name="dec3", dropout=0.3)
         self.upconv2 = nn.ConvTranspose2d(features * 4, features * 2, kernel_size=2, stride=2)
-        self.decoder2 = BrainRegNet._block((features * 2) * 2, features * 2, name="dec2")
+        self.decoder2 = BrainRegNet._block((features * 2) * 2, features * 2, name="dec2", dropout=0.3)
         self.upconv1 = nn.ConvTranspose2d(features * 2, features, kernel_size=2, stride=2)
-        self.decoder1 = BrainRegNet._block(features * 2, features, name="dec1")
+        self.decoder1 = BrainRegNet._block(features * 2, features, name="dec1", dropout=0.3)
         
         self.conv = nn.Conv2d(in_channels=features, out_channels=out_channels, kernel_size=1)
 
-    def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
-        
-        bottleneck = self.bottleneck(self.pool4(enc4))
-        
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.decoder4(dec4)
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
-        
-        return self.conv(dec1)
-
     @staticmethod
-    def _block(in_channels, features, name):
+    def _block(in_channels, features, name, dropout=0.0):
         return nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=features, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(num_features=features),
             nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
             nn.Conv2d(in_channels=features, out_channels=features, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(num_features=features),
             nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
         )
+
 
 
 class SSIM_Loss(SSIM):
@@ -94,6 +74,16 @@ def create_grid(batch_size, shape):
     grid = grid.unsqueeze(0).expand(N, -1, -1, -1)
     return grid
 
+class AddGaussianNoise(object):
+    def __init__(self, mean=0.0, std=1.0):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 class PairedDataset(Dataset):
     def __init__(self, originals, targets, transform=None):
         self.originals = originals
@@ -159,8 +149,11 @@ def train(rank, world_size, args):
     transform = transforms.Compose(
         [
             transforms.ToPILImage(),
-            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+            transforms.RandomRotation(10),
+            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
+            AddGaussianNoise(0.0, 0.1),
         ]
     )
 
@@ -174,6 +167,7 @@ def train(rank, world_size, args):
     model = DDP(model, device_ids=[rank])
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     ssim_loss = SSIM_Loss(data_range=1.0, size_average=True, channel=1).to(rank)
 
     best_loss = float("inf")
@@ -208,7 +202,8 @@ def train(rank, world_size, args):
                 print(f"Epoch: {epoch}, Batch: {batch + 1} / {len(dataloader)}, Loss: {train_loss / num_samples:.4f}")
 
         epoch_loss = train_loss / num_samples
-
+        scheduler.step()
+        
         if epoch_loss < best_loss:
             best_loss = epoch_loss
             if rank == 0:  # Only save on rank 0 to avoid overwriting
